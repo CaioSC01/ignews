f32b996b96515bef9062c146946ba3743fad083e
CaioSC01 <caio180g@gmail.com>
2023-01-25 02:43:38 -0300

    finalizando botão de signin

--- src/components/SinginButton/index.tsx
+++ src/components/SinginButton/index.tsx
@@ -1,9 +1,25 @@
 import React from "react";
+import { FaGithub } from "react-icons/fa";
+import { FiX } from "react-icons/fi";
+import styles from "./style.module.scss";
 
 export function SingInButton() {
-  return(
-		<>
-		<button type="button"></button>
-		</>
-	);
+  const isUserLoggedIn = true;
+
+  return isUserLoggedIn ? (
+    <>
+      <button type="button" className={styles.signInButton}>
+        <FaGithub color="#04d361" />
+        Caio silva
+        <FiX color="#737380" className={styles.closeIcon} />
+      </button>
+    </>
+  ) : (
+    <>
+      <button type="button" className={styles.signInButton}>
+        <FaGithub color="#eba417" />
+        Sign in With GitHub
+      </button>
+    </>
+  );
 }
